e350742f754eca6de741b4de80d1f56d889ae553
jcd1141 <joshd3887@gmail.com>
2026-02-09 21:11:58 +0000

    Export all classes from package fixed

--- src/workout_tracker/__init__.py
+++ src/workout_tracker/__init__.py
@@ -1,10 +1,19 @@
 """Workout Tracker - A simple OOP-based workout tracking system."""
 
-from workout_tracker.exercises import Exercise, CardioExercise
+from workout_tracker.exercises import (
+    Exercise,
+    CardioExercise,
+    StrengthExercise,
+    FlexibilityExercise
+)
+from workout_tracker.workout import Workout
 
 __version__ = "0.1.0"
 
 __all__ = [
     "Exercise",
     "CardioExercise",
+    "StrengthExercise",
+    "FlexibilityExercise",
+    "Workout"
 ]
